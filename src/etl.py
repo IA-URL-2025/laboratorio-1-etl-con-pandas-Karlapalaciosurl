5cda6756bfa5127515aca7cba6ccb81004035780
Karlapalaciosurl <70666525+Karlapalaciosurl@users.noreply.github.com>
2026-02-04 14:35:03 -0600

    Update etl.py

--- src/etl.py
+++ src/etl.py
@@ -6,7 +6,21 @@ def run_etl():
     No cambies el nombre de esta función.
     """
     # TODO: implementar
-    pass
+    #Extraer
+    citas = pd.read_csv("data/citas_clinica.csv")
+
+    citas["paciente"] = citas["paciente"].str.title()
+    citas["especialidad"] = citas["especialidad"].str.upper()
+
+    citas["fecha_cita"] = pd.to_datetime(citas["fecha_cita"], errors="coerce")
+    citas = citas.dropna(subset=["fecha_cita"])
+
+    citas = citas[citas["estado"] == "CONFIRMADA"]
+    citas = citas[citas["costo"] > 0]
+
+    citas["telefono"] = citas["telefono"].fillna("NO REGISTRA")
+
+    citas.to_csv("data/output.csv", index=False)
 
 
 if __name__ == "__main__":
